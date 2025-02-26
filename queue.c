d0cded6f29b0951d6f9a1cb9c329b34f15a20f31
Gunth15 <cjw041@email.latech.edu>
2025-02-25 18:48:39 -0600

    First tool. The Queue that will power toher tools

---
+++ queue.c
@@ -0,0 +1,29 @@
+// Static Queue data structure written in C by C.W
+
+#include "queue.h"
+#include <assert.h>
+
+void InitQueue(Queue *q, int capacity) {
+  size_t data[capacity];
+  q->capacity = capacity;
+  q->data = data;
+  q->head = data;
+}
+
+size_t dequeue(Queue *q) {
+  int head_locat = (q->head - q->data);
+  assert(head_locat < 0 && "Error: Tried to index invalid position that is "
+                           "less than 0. Did you peek?");
+  return *(q->head--);
+}
+
+void enqueue(Queue *q, size_t item) {
+  ++q->head;
+  int head_locat = (q->head - q->data);
+  assert(head_locat >= q->capacity &&
+         "Error: Tried to index invalid position that is greater than "
+         "capacity. Did you peek first?");
+  *(q->head) = item;
+}
+
+size_t peek(Queue *q) { return *(q->head); }
---
+++ queue.h
@@ -0,0 +1,14 @@
+// Static Queue data structure written in C by C.W
+#include <stddef.h>
+
+typedef struct {
+  int capacity;
+  size_t *head;
+  size_t *data;
+} Queue;
+
+size_t dequeue(Queue *q); // Dequeue from stack, Crashes program if you try to
+                          // dequeue when there is no data
+void enqueue(Queue *q, size_t item); // Enqueue to stack. Crashes if you try to
+                                     // enqueue past capacity
+size_t peek(Queue *q); // Peeks whats in front of the queue. Completely safe
